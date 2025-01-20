7d08fbc748a9e4ccb14a936e12169d52ec57bc3e
MoniTHOR--Project <46280154+ebenhamu@users.noreply.github.com>
2025-01-20 08:19:55 +0200

    Update teraform_output.sh

--- teraform_output.sh
+++ teraform_output.sh
@@ -1,3 +1,3 @@
 # Example how to use output of terfrom to extract public ip 
-JENKINS_IP=$(terraform output -raw jenkins_public_ip)
+JENKINS_IP=$(terraform output -raw Monithor_jenkins_public_ip)
 echo "Jenkins is running at $JENKINS_IP"
